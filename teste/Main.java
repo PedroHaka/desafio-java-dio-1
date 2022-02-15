636db2e1627a7fdf2e03a065527d1e8d46636d84
Pedro_Haka <pedrohaka94@gmail.com>
2022-02-15 18:38:40 -0300

    Initial test commit

---
+++ teste/Main.java
@@ -0,0 +1,15 @@
+package teste;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        String var1 = "Hello";
+        String var2 = "World";
+
+        //creating an object "Teste"
+        Teste compliment = new Teste(var1, var2);
+
+        //calling functions/methods
+        compliment.toString();
+    }
+}
---
+++ teste/Teste.java
@@ -0,0 +1,23 @@
+package teste;
+
+public class Teste{
+    //atributes
+    public String var1;
+    public String var2;
+
+    //methods
+    //empty constructor
+    public Teste(){}
+
+
+    public Teste(String A, String B){
+        this.var1 = A;
+        this.var2 = B;
+    }
+
+    @Override
+    public String toString(){
+        return "HAHAHAHAHAHAHA";
+    }
+    
+}
