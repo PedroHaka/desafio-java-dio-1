9f6a9cb978784b37e82a372115694e57f6b51049
Pedro Haka <70613448+PedroHaka@users.noreply.github.com>
2022-02-20 12:25:32 -0300

    Delete bancodigitaldio directory

--- bancodigitaldio/InterfaceConta.java
+++
@@ -1,7 +0,0 @@
-package bancodigitaldio;
-
-public interface InterfaceConta {
-    /*public*/ void deposit(double value);
-    void withdraw(double value);
-    void transfer(double value, Conta destinationAccount); 
-}
